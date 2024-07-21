51b7ebd43557642ac2e84f430cd937a09972d002
U2665ofEvil <79206160705@ya.ru>
2024-07-21 21:49:05 +0300

    homework upd

--- homework/module8/zero.py
+++ homework/module8/zero.py
@@ -1,13 +1,16 @@
-v = int(input('enter v: '))
-sum_v = 0
-cout = 0
-while v > sum_v:
-    a = int(input('something: '))
-    sum_v += a
-    if sum_v > v:
-        sum_v -= a
+n, k = map(int, input().split())
+# n и k (1 ≤ n ≤ 10, 1 ≤ k ≤ 240)
+a = 240 - k  # 'а' времени остаётся на n задач
+b = 0  # время потраченное на задачи
+#t = 5  # 5 минут на первую задачу
+r = 0  # количество решенных задач
+while b < a:
+    for i in range(1, n+1):
+        b = b + i * 5
+        if b <= a:
+            r += 1
+        else:
+            break
+    if r >= n:
         break
-    cout += 1
-print('Довольно!')
-print(sum_v)
-print(cout)
+print(r)
