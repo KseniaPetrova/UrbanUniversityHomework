818378f0a6a2a73c410401b1f6f215ac4d379a97
U2665ofEvil <79206160705@ya.ru>
2024-07-23 23:00:37 +0300

    homework upd

--- homework/module9/zero.py
+++ homework/module9/zero.py
@@ -1,20 +1,6 @@
-def all_variants(text):
-    """
-    Функция-генератор, которая возвращает все подпоследовательности переданной строки.
-    """
-    # Создаем список всех возможных длин подпоследовательностей
-    lengths = list(range(1, len(text) + 1))
+def my_function():
+    for i in range(1, 5):
+        print(i)
+        return i
 
-    # Проходим по всем возможным длинам
-    for length in lengths:
-        # Проходим по всем возможным начальным индексам
-        for start in range(len(text) - length + 1):
-            # Возвращаем подпоследовательность
-            yield text[start:start + length]
-
-
-a = all_variants("abc")
-for i in a:
-    print(i)
-
-print(len('abc'))
+print(my_function())
