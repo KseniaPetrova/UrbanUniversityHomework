fe47ea83f05383bdc95a32c7b14f224442f51ceb
U2665ofEvil <79206160705@ya.ru>
2024-11-01 18:33:43 +0300

    homework upd

--- homework/module17/homework/app/main.py
+++ homework/module17/homework/app/main.py
@@ -7,6 +7,9 @@ from sqlalchemy.schema import CreateTable
 app = FastAPI()
 # uvicorn homework.module17.homework.app.main:app
 # 127.0.0.1:8000/docs
+# находясь в дериктории UrbanUniversityHomework\homework\module17\homework>
+# alembic init app/migrations
+# alembic revision --autogenerate -m "Initial migration"
 
 @app.get("/")
 async def welcome():
